93892d84b299208dc07b885241253d7b9e11ed90
Ashley Jeffs <ash@jeffail.uk>
2021-09-29 18:49:48 +0100

    Add base component imports to plugin example

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/Jeffail/benthos/v3/public/service"
 
+	// Import all standard Benthos components
+	_ "github.com/Jeffail/benthos/v3/public/components/all"
+
 	// Add your plugin packages here
 	_ "github.com/benthosdev/benthos-plugin-example/bloblang"
 	_ "github.com/benthosdev/benthos-plugin-example/cache"
